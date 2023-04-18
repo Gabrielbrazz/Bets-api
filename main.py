623e17a0a6ab9ee9e79322408a97b734b7f7c313
Gabriel <gabodobrasil123@gmail.com>
2023-04-18 08:36:20 -0300

    puxando jogos da dinamarca

--- main.py
+++ main.py
@@ -2,106 +2,120 @@ import os
 import requests
 import pandas as pd
 
-
+# até agora o código está puxando a api e colocando muuitos dados no prompt
 
 api_key = os.getenv("API_KEY")
 API_KEY ='b6a13693bb25fc01d346c527c6a24035'
 
-SPORT = 'upcoming' # use the sport_key from the /sports endpoint below, or use 'upcoming' to see the next 8 games across all sports
+SPORT = 'soccer_denmark_superliga' # use the sport_key from the /sports endpoint below, or use 'upcoming' to see the next 8 games across all sports
 
 REGIONS = 'uk' # uk | us | eu | au. Multiple can be specified if comma delimited
 
-MARKETS = 'h2h' # h2h | spreads | totals. Multiple can be specified if comma delimited
+MARKETS = 'h2h' 
 
-ODDS_FORMAT = 'decimal' # decimal | american
+ODDS_FORMAT = 'decimal' 
 
-DATE_FORMAT = 'iso' # iso | unix
+DATE_FORMAT = 'iso' 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#
-# First get a list of in-season sports
-#   The sport 'key' from the response can be used to get odds in the next request
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+BET_SIZE = 10
 
-sports_response = requests.get(
-    'https://api.the-odds-api.com/v4/sports', 
-    params={
-        'api_key': API_KEY
-    }
-)
-
-
-if sports_response.status_code != 200:
-    print(f'Failed to get sports: status_code {sports_response.status_code}, response body {sports_response.text}')
-
-else:
-    print('List of in season sports:', sports_response.json())
-
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#
-# Now get a list of live & upcoming games for the sport you want, along with odds for different bookmakers
-# This will deduct from the usage quota
-# The usage quota cost = [number of markets specified] x [number of regions specified]
-# For examples of usage quota costs, see https://the-odds-api.com/liveapi/guides/v4/#usage-quota-costs
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
+#puxa as odds da api
 odds_response = requests.get(
-    f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds',
-    params={
+    f'https://api.the-odds-api.com/v4/sports/soccer_denmark_superliga/odds',
+    data={
         'api_key': API_KEY,
         'regions': REGIONS,
         'markets': MARKETS,
         'oddsFormat': ODDS_FORMAT,
         'dateFormat': DATE_FORMAT,
     }
-)
+).json()
 
-if odds_response.status_code != 200:
-    print(f'Failed to get odds: status_code {odds_response.status_code}, response body {odds_response.text}')
+print(odds_response)
 
-else:
-    odds_json = odds_response.json()
-    print('Number of events:', len(odds_json))
-    print(odds_json)
+BOOKMAKER_INDEX = 0
+NAME_INDEX = 1
+ODDS_INDEX = 2
+FIRST = 0
 
-    # Check the usage quota
-    print('Remaining requests', odds_response.headers['x-requests-remaining'])
-    print('Used requests', odds_response.headers['x-requests-used'])
+class Event:
+    def __init__(self, data):
+        self.data = data
+        self.sport_key = data['sport_key']
+        self.id = data['id']
+        
+    def find_best_odds(self):
+        # number of possible outcomes for a sporting event
+        num_outcomes = len(self.data['bookmakers'][FIRST]['markets'][FIRST]['outcomes'])
+        self.num_outcomes = num_outcomes
 
+        # finding the best odds for each outcome in each event
+        best_odds = [[None, None, float('-inf')] for _ in range(num_outcomes)]
+        # [Bookmaker, Name, Price]
 
-# Organizando os dados recebidos da API com Pandas
+        bookmakers = Event.data['bookmakers']
+        for index, bookmaker in enumerate(bookmakers):
 
-# Dados da API
-data = {
-    'success': True,
-    'data': [
-        {
-            'sport_key': 'soccer_epl',
-            'teams': ['Arsenal', 'Everton'],
-            'commence_time': 1610296800,
-            'sites': [
-                {
-                    'site_key': 'betfair',
-                    'site_nice': 'Betfair',
-                    'last_update': 1610288638,
-                    'odds': {
-                        'h2h': [2.2, 3.3, 3.9]
-                    }
-                }
-            ]
-        },
-        # ...
-    ]
-}
+            # determing the odds offered by each bookmaker
+            for outcome in range(num_outcomes):
 
-# Transformando os dados em um DataFrame
-df = pd.json_normalize(data['data'], 'sites', ['teams', 'commence_time', 'sport_key', ], record_prefix='site_')
-df['odd'] = df['odds.h2h'].apply(lambda x: x[0]) # Adicionando a odd de casa (home) como uma coluna
-df['odd_away'] = df['odds.h2h'].apply(lambda x: x[1]) # Adicionando a odd de visitante (away) como uma coluna
+                # determining if any of the bookmaker odds are better than the current best odds
+                bookmaker_odds = float(bookmaker['markets'][FIRST]['outcomes'][outcome]['price'])
+                current_best_odds = best_odds[outcome][ODDS_INDEX]
 
-# Visualizando o DataFrame
-print(df)
+                if bookmaker_odds > current_best_odds:
+                    best_odds[outcome][BOOKMAKER_INDEX] = bookmaker['title']
+                    best_odds[outcome][NAME_INDEX] = bookmaker['markets'][FIRST]['outcomes'][outcome]['name']
+                    best_odds[outcome][ODDS_INDEX] = bookmaker_odds
+                    
+        self.best_odds = best_odds
+        return best_odds
+    
+    def arbitrage(self):
+        total_arbitrage_percentage = 0
+        for odds in self.best_odds:
+            total_arbitrage_percentage += (1.0 / odds[ODDS_INDEX])
+            
+        self.total_arbitrage_percentage = total_arbitrage_percentage
+        self.expected_earnings = (BET_SIZE / total_arbitrage_percentage) - BET_SIZE
+        
+        # if the sum of the reciprocals of the odds is less than 1, there is opportunity for arbitrage
+        if total_arbitrage_percentage < 1:
+            return True
+        return False
+    
+    # converts decimal/European best odds to American best odds
+    def convert_decimal_to_american(self):
+        best_odds = self.best_odds
+        for odds in best_odds:
+            decimal = odds[ODDS_INDEX]
+            if decimal >= 2:
+                american = (decimal - 1) * 100
+            elif decimal < 2:
+                american = -100 / (decimal - 1)
+            odds[ODDS_INDEX] = round(american, 2)
+        return best_odds
+     
+    def calculate_arbitrage_bets(self):
+        bet_amounts = []
+        for outcome in range(self.num_outcomes):
+            individual_arbitrage_percentage = 1 / self.best_odds[outcome][ODDS_INDEX]
+            bet_amount = (BET_SIZE * individual_arbitrage_percentage) / self.total_arbitrage_percentage
+            bet_amounts.append(round(bet_amount, 2))
+        
+        self.bet_amounts = bet_amounts
+        return bet_amounts
+    
+    Event = []
+for data in odds_response:
+    Event.append(Event(data))
+    
+arbitrage_events = []
+for event in Event:
+    best_odds = event.find_best_odds()
+    if event.arbitrage():
+        arbitrage_events.append(event)
+        
+for event in arbitrage_events:
+    event.calculate_arbitrage_bets()
+    event.convert_decimal_to_american()
